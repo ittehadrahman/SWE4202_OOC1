48b957ef040280164cbd821f717edfe868a02dac
ittehadrahman <95122519+ittehadrahman@users.noreply.github.com>
2022-03-21 20:16:31 +0600

    Add files via upload

---
+++ Pharmacy Management Sysytem/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Management_Sysytem
+{
+    public class Account
+    {
+        public int accountbalance;
+
+        public List<Medicine> medicines = new List<Medicine>();
+        
+    }
+}
---
+++ Pharmacy Management Sysytem/Medicine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy_Management_Sysytem
+{
+    public class Medicine
+    {
+        public string name;
+        public int price;
+        public int quantity;
+        public int previous_stock;
+        public void add()
+        {
+            this.previous_stock = this.quantity + this.previous_stock;
+        }
+        public void sell()
+        {
+            this.previous_stock = this.previous_stock - this.quantity ;
+        }
+        public string getInfo()
+        {
+            string medicineinfo = name + "\t" + Convert.ToString(previous_stock);
+            return medicineinfo;
+        }
+
+    }
+}
